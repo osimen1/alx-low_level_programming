b01dc985a50c27ba51daa703e8efacde8c7abd07
osimen <osimenvictor@gmail.com>
2023-02-17 12:55:52 -0800

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print has function
+ *
+ * Return: alway 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+
+}
